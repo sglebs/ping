1a976e17edee2ab3935a8ba025aabcbc3df0bce8
Marcio Marchini <mqm@sglebs.com>
2016-06-24 14:22:06 -0300

    s101 publishing in separate stage

--- s101.sh
+++ s101.sh
@@ -1,7 +1,7 @@
-if [ "$(java -Xmx512m -Djava.library.path=/Users/mqm/s101-build -jar /Users/mqm/s101-build/structure101-gen-build.jar s101.build.xml 2>&1 | tee s101-out.txt | grep -cim1 SEVERE)" -ge 1 ]; then
-  cat s101-out.txt
+if [ "$(java -Xmx512m -Djava.library.path=/Users/mqm/s101-build -jar /Users/mqm/s101-build/structure101-gen-build.jar $1 2>&1 | tee $1-out.txt | grep -cim1 SEVERE)" -ge 1 ]; then
+  cat $1-out.txt
   exit 1
 else
-  cat s101-out.txt
+  cat $1-out.txt
   exit 0
 fi
