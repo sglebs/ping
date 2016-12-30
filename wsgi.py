9188e639aaeab0694a54f0a0f954dc2d5b22f965
Marcio Marchini <mqm@sglebs.com>
2016-12-30 15:16:15 -0200

    newer reqs

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
-from bottle import default_app
+from bottle import default_app, run
 
 application = default_app()
 import controllers.ping # necessary so that Bottle will install its routes
+
+if __name__ == '__main__':
+    run(application, host='0.0.0.0', port=9090)
