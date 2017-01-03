00df40ab64ef89d58e48533322a71277e85e891a
Marcio Marchini <mqm@sglebs.com>
2017-01-03 13:58:58 -0200

    with non-web cmd

--- stress/run.sh
+++ stress/run.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-locust -f ./locustfile.py --host=http://localhost:8080
+#
+# web UI:
+#locust -f ./locustfile.py --host=http://localhost:8080
+#
+locust -f ./locustfile.py --host=http://localhost:8080 --clients=500 --hatch-rate=20 --num-request=5000 --no-web --only-summary  > locust.log   2>&1
