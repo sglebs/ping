9ab521a876afb3f6ca6ad008a9b5b12b1016bd4b
Marcio Marchini <mqm@sglebs.com>
2017-01-04 08:32:04 -0200

    ulimit -n 4096

--- stress/run.sh
+++ stress/run.sh
@@ -3,4 +3,6 @@
 # web UI:
 #locust -f ./locustfile.py --host=http://localhost:8080
 #
+# bump the number of hanfdles we can open
+ulimit -n 4096
 locust -f ./locustfile.py --host=http://localhost:8080 --clients=500 --hatch-rate=20 --num-request=5000 --no-web --only-summary  > locust.log   2>&1
